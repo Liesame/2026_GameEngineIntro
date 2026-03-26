da1d85a4ab215c35c4efef2720472ad7d3707b69
Liesame <aaa01062223008@gmail.com>
2026-03-26 16:11:12 +0900

    Add: 트리거, 스크린 이동, UI 만들기

---
+++ Test/Assets/TitieManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitieManager : MonoBehaviour
+{
+
+    public GameObject helpPanel;
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("PlayScene_Door1");
+    }
+
+    public void OpenHelp()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void GoTitie()
+    {
+        SceneManager.LoadScene("TitieScene");
+    }
+    
+
+
+}
